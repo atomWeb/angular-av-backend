7109602ac42548e1dd13f95a193b76f0644748c8
Nabusim <cristianjose@gmail.com>
2021-08-08 12:31:44 +0200

    Resto codigo para servir SPA

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 require("dotenv").config();
-
+const path = require("path");
 const express = require("express");
 const cors = require("cors");
 
@@ -27,6 +27,12 @@ app.use("/api/doctors", require("./routes/doctors"));
 app.use("/api/login", require("./routes/auth"));
 app.use("/api/all", require("./routes/searches"));
 app.use("/api/uploads", require("./routes/uploads"));
+
+// Para servir en la ruta static una SPA, se despliega alli el contenido de dist.
+// Y ademas lo siguiente:
+// app.get("*", (req, res) => {
+//   res.sendFile(path.resolve(__dirname, "public/index.html"));
+// });
 
 app.listen(process.env.PORT, () => {
   console.log("Api run on port ", process.env.PORT);
